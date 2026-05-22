d7e9976ce46308d3457c972a91e83e919ed75610
getkrowd-del <getkrowd@gmail.com>
2026-05-22 12:23:08 -0400

    Deploy landing page: Revolutionary Wellness - Home

---
+++ script.js
@@ -0,0 +1,2 @@
+const obs = new IntersectionObserver(entries => entries.forEach(e => { if(e.isIntersecting) e.target.classList.add('visible'); }), { threshold:0.1, rootMargin:'0px 0px -60px 0px' });
+document.querySelectorAll('.fade-up').forEach(el => obs.observe(el));
